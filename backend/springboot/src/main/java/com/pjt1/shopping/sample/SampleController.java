69b4626013e7fffd2f0732427273c16d3b43d57b
강유정 <kos9811@naver.com>
2021-01-21 14:11:02 +0900

    swagger, properties 세팅

---
+++ backend/springboot/src/main/java/com/pjt1/shopping/sample/SampleController.java
@@ -0,0 +1,31 @@
+package com.pjt1.shopping.sample;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping(value = "/addition")
+	public Integer addition(Integer num1, Integer num2) {
+		return num1 + num2;
+	}
+
+	@PostMapping(value = "/subtraction")
+	public Integer subtraction(Integer num1, Integer num2) {
+		return num1 - num2;
+	}
+
+	@PutMapping(value = "/multiplication")
+	public Integer multiplication(Integer num1, Integer num2) {
+		return num1 * num2;
+	}
+
+	@DeleteMapping(value = "/division")
+	public Integer division(Integer num1, Integer num2) {
+		return num1 / num2;
+	}
+}
