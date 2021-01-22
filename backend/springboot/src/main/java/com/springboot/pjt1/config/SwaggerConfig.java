ab77e714ea6b913c3c40c50fef9d8b7bf1bdc328
강유정 <kos9811@naver.com>
2021-01-22 16:57:35 +0900

    상품검색 이마트 product로 완료

--- backend/springboot/src/main/java/com/springboot/pjt1/config/SwaggerConfig.java
+++ backend/springboot/src/main/java/com/springboot/pjt1/config/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package com.springboot.pjt1.config;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicates;
+
+import io.swagger.models.Contact;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,8 +20,16 @@ public class SwaggerConfig {
 	// http://localhost:8000/springboot/swagger-ui.html
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+        		.apiInfo(this.apiInfo())
+        		.select()
                 .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .paths(PathSelectors.any()).build();
     }
+    
+    private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("장보기 API")
+				.description("<h3>장보기 API Reference for Developers</h3> 장보기 API입니다. 모두들 파이팅 ^0^")
+				.build();
+	}
 }
