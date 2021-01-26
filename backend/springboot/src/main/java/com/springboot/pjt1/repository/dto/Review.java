76ccaf2298c2812383d511aa866f062be73eb711
강유정 <kos9811@naver.com>
2021-01-26 16:37:56 +0900

    이마트 리뷰 크롤링 완료

---
+++ backend/springboot/src/main/java/com/springboot/pjt1/repository/dto/Review.java
@@ -0,0 +1,34 @@
+package com.springboot.pjt1.repository.dto;
+
+public class Review {
+	private int product_no;
+	private String score;
+	private String comment;
+	private String date;
+	
+	public int getProduct_no() {
+		return product_no;
+	}
+	public void setProduct_no(int product_no) {
+		this.product_no = product_no;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
