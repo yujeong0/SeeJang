2a572b716ff128bcd4ab93c02f86e7a9f50dde45
NICEMANEUL <cndtjq22@gmail.com>
2021-01-27 01:22:34 +0900

    프론트엔드 네이버 API 연결 시도1

---
+++ frontend/cart/src/page/Login_callback.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout column> </v-layout>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const that = this;
+    const naverLogin = new naver.LoginWithNaverId({
+      clientId: 'clientId',
+      isPopup: false,
+    });
+    naverLogin.init();
+    naverLogin.getLoginStatus(function (status) {
+      if (status) {
+        const email = naverLogin.user.getEmail();
+        const name = naverLogin.user.getName();
+        const profileImage = naverLogin.user.getProfileImage();
+        const birthday = naverLogin.user.getBirthday();
+        const uniqId = naverLogin.user.getId();
+        const age = naverLogin.user.getAge();
+        console.log(email);
+        console.log(name);
+        console.log(profileImage);
+        console.log(birthday);
+        console.log(uniqId);
+        console.log(age);
+      } else {
+        console.log('AccessToken이 올바르지 않습니다.');
+      }
+      that.$router.push({ name: 'login' });
+    });
+  },
+};
+</script>
