20980532d88a674674e82ba66a204eb2c131e6b7
정준영 <dkwjdiq@naver.com>
2021-01-27 16:04:59 +0900

    product Img 폴더생성 및 삽입

--- frontend/cart/src/store/index.js
+++ frontend/cart/src/store/index.js
@@ -1,11 +1,27 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    detail: {
+      product_name: '',
+      product_price: '',
+      link: '',
+      score: '',
+      comment :''
+    }
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    getProductName(state) {
+      return state.product_name;
+    },
+    getProductPrice(state) {
+      return state.product_price;
+    },
+  },
 });
