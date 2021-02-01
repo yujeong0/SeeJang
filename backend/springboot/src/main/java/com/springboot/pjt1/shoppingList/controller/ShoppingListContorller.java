2d28448fa4b442b1b2d53183dfdf882bc9da3e16
강민창 <new0822@naver.com>
2021-02-01 23:09:16 +0900

    crossOrigin 문제 해결

--- backend/springboot/src/main/java/com/springboot/pjt1/shoppingList/controller/ShoppingListContorller.java
+++ backend/springboot/src/main/java/com/springboot/pjt1/shoppingList/controller/ShoppingListContorller.java
@@ -19,7 +19,7 @@ import com.springboot.pjt1.shoppingList.service.ShoppingListService;
 
 import io.swagger.annotations.ApiOperation;
 
-@CrossOrigin(origins = { "*" })
+@CrossOrigin(origins = { "http://localhost:8080" }, allowCredentials = "true")
 @RestController
 @RequestMapping("/shoppingList")
 public class ShoppingListContorller {
