5c661f89cf3d0e81daf3ae2899ac2ff1fea05f4d
강유정 <kos9811@naver.com>
2021-02-02 11:21:24 +0900

    wishlist 완료, review 수정, 최저가 크롤링 수정 완료

--- backend/springboot/src/main/java/com/springboot/pjt1/repository/dto/ProductReview.java
+++ backend/springboot/src/main/java/com/springboot/pjt1/repository/dto/ProductReview.java
@@ -1,19 +1,26 @@
 package com.springboot.pjt1.repository.dto;
 
 public class ProductReview {
-	private int comment_no;
-	private int product_no;
+	
+	private int commentNo;
+	private int productNo;
 	private String memberId;
 	private String memberName;
 	private String score;
 	private String comment;
 	private String date;
 	
-	public int getComment_no() {
-		return comment_no;
+	public int getCommentNo() {
+		return commentNo;
 	}
-	public void setComment_id(int comment_no) {
-		this.comment_no = comment_no;
+	public void setCommentNo(int commentNo) {
+		this.commentNo = commentNo;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
 	}
 	public String getMemberId() {
 		return memberId;
@@ -26,12 +33,6 @@ public class ProductReview {
 	}
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
-	}
-	public int getProduct_no() {
-		return product_no;
-	}
-	public void setProduct_no(int product_no) {
-		this.product_no = product_no;
 	}
 	public String getScore() {
 		return score;
@@ -51,11 +52,7 @@ public class ProductReview {
 	public void setDate(String date) {
 		this.date = date;
 	}
-	@Override
-	public String toString() {
-		return "ProductReview [comment_no=" + comment_no + ", product_no=" + product_no + ", memberId=" + memberId
-				+ ", memberName=" + memberName + ", score=" + score + ", comment=" + comment + ", date=" + date + "]";
-	}
-
+	
+	
 	
 }
