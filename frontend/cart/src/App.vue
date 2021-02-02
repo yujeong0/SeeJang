f4ca570409754d7b65b4240a357beb530022fca2
YSM-samsungNB <cndtjq145@naver.com>
2021-02-02 18:36:44 +0900

    프론트엔드 - 모드 나눔 추가

--- frontend/cart/src/App.vue
+++ frontend/cart/src/App.vue
@@ -1,38 +1,37 @@
 <template>
-  <v-app>
-    <Header v-if="this.$store.getters.getIsLogin || isLogin" ></Header>
-    <router-view />
-  </v-app>
+    <v-app>
+        <Header v-if="this.$store.getters.getIsLogin || isLogin"></Header>
+        <router-view />
+    </v-app>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
 export default {
-  name: 'App',
-
-  components: {
-    Header,
-  },
-  updated(){
-    this.isLogin = sessionStorage.getItem('isLogin')
-  },
-  data(){
-    return {
-      isLogin : sessionStorage.getItem('isLogin')
-    };
-  },
+    name: "App",
+    created(){
+      console.log(this.isLogin);
+    },
+    components: {
+        Header,
+    },
+    data() {
+        return {
+            isLogin: sessionStorage.getItem("isLogin"),
+        };
+    },
 };
 </script>
 
 <style>
 #container {
-  font-family: 'Do Hyeon', sans-serif;
+    font-family: "Do Hyeon", sans-serif;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
--- frontend/cart/src/page/ModeSetting.vue
+++ frontend/cart/src/page/ModeSetting.vue
@@ -6,12 +6,12 @@
         <br />
         <br />
         <v-btn rounded color="#7986CB" dark large class="btn" @click="blindMode">
-            <v-text class="mode">시각장애인 모드</v-text>
+            <label class="mode">시각장애인 모드</label>
         </v-btn>
         <br />
         <br />
         <v-btn rounded color="#7CB342" dark large class="btn" @click="notblindMode">
-            <v-text class="mode">비장애인 모드</v-text>
+            <label class="mode">비장애인 모드</label>
         </v-btn>
     </div>
 </template>
