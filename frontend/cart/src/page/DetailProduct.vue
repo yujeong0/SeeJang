29258b2f99b1a433a67f9870060e443273e771e3
정준영 <dkwjdiq@naver.com>
2021-02-04 21:38:57 +0900

    평점사진, 상품검색결과화면 / 일단 비비고 왕교자로 해놨음

---
+++ frontend/cart/src/page/DetailProduct.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Info></Info>
+    <CheapPriceLink></CheapPriceLink>
+  </div>
+</template>
+
+<script>
+import Info from '@/components/detail/Info.vue';
+import CheapPriceLink from '@/components/detail/CheapPriceLink.vue';
+import ReviewList from '@/components/detail/ReviewList.vue';
+export default {
+  components: {
+    Info,
+    CheapPriceLink,
+    ReviewList,
+  },
+};
+</script>
+
+<style scoped></style>
