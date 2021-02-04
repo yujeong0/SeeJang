61a3c659e565bf51ecb7567033bacf094a169be8
YSM-samsungNB <cndtjq145@naver.com>
2021-02-04 10:28:55 +0900

    프론트엔드 - 쇼핑리스트 추가 구현 _ 수정 진행중 _ 로그인 임시 넘김적용

--- frontend/cart/src/components/item.vue
+++ frontend/cart/src/components/item.vue
@@ -1,62 +1,78 @@
 <template>
-  <div class="itemWrap" id="container">
-    <v-container>
-      <v-row no-gutters>
-        <v-col>
-          <input type="checkbox" :checked="item.checked" />
-        </v-col>
-        <v-col style="text-align: center">
-          {{ item.productName }}
-        </v-col>
-        <v-col style="text-align: right"> ₩ {{ item.productPrice }} </v-col>
-        <v-col style="text-align: right; width: 2%">
-          <v-btn text @click="del" depressed color="error"> del </v-btn></v-col
-        >
-      </v-row>
-    </v-container>
-  </div>
+    <div class="itemWrap" id="container">
+        <v-container>
+            <v-row no-gutters>
+                <v-col>
+                    <input type="checkbox" v-model="checked" :checked="checked" @click="check" />
+                </v-col>
+                <v-col style="text-align: center">
+                    {{ productName }}
+                </v-col>
+                <v-col style="text-align: right"> ₩ {{ productPrice }} </v-col>
+                <v-col style="text-align: right; width: 2%">
+                    <v-btn text @click="del" depressed color="error"> del </v-btn></v-col
+                >
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
-import http from '@/util/http-common.js';
+import http from "@/util/http-common.js";
 export default {
-  name: 'item',
-  props: {
-    item: Object,
-  },
-  data() {
-    return {};
-  },
-  created() {
-    let money = parseInt(this.item.productPrice);
-    this.$store.commit('ADD_TOTAL_MONEY', { money });
-  },
-  methods: {
-    del() {
-      let money = parseInt(this.item.productPrice);
-      http
-        .delete('/shoppingList', {
-          params: {
-            shoppingListNo: this.item.shoppingListNo,
-          },
-          withCredentials: true,
-        })
-        .then((response) => {
-          this.$store.commit('DEL_ITEM', { money });
-          console.log(response);
-          this.$emit('del', this.item.shoppingListNo);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    name: "item",
+    props: {
+        item: Object,
     },
-  },
+    data() {
+        return {
+            checked: this.item.checked,
+            productName: this.item.productName,
+            productPrice: this.item.productPrice,
+            shoppingListNo: this.item.shoppingListNo,
+        };
+    },
+    created() {
+        if (this.checked) {
+            let money = parseInt(this.productPrice);
+            this.$store.commit("ADD_TOTAL_MONEY", { money });
+        }
+    },
+    methods: {
+        check() {
+            let money = parseInt(this.productPrice);
+            if (!this.checked) {
+                this.$store.commit("ADD_TOTAL_MONEY", { money });
+            } else {
+                this.$store.commit("DEL_ITEM", { money });
+            }
+        },
+        del() {
+            let money = parseInt(this.productPrice);
+            http.delete("/shoppingList", {
+                params: {
+                    shoppingListNo: this.shoppingListNo,
+                },
+                withCredentials: true,
+            })
+                .then((response) => {
+                    if (this.checked) {
+                        this.$store.commit("DEL_ITEM", { money });
+                    }
+                    console.log(response);
+                    this.$emit("del", this.shoppingListNo);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+    },
 };
 </script>
 
 <style scope>
 .itemWrap {
-  margin: 1% 10%;
-  text-align: left;
+    margin: 1% 10%;
+    text-align: left;
 }
 </style>
