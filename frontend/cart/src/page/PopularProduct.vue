52af90ee1c7e66a4437b5fe07f39afd8d804d85f
YSM-samsungNB <cndtjq145@naver.com>
2021-02-04 21:47:33 +0900

    프론트엔드 - 카테고리 구현 1

--- frontend/cart/src/page/PopularProduct.vue
+++ frontend/cart/src/page/PopularProduct.vue
@@ -1,15 +1,14 @@
 <template>
-  <div id="app" class="columns mt-4">
-    <categorize />
-    <br />
-    <h3 class="mb-5">이번 주 인기상품</h3>
-    <div class="column">
-      <popular
-        v-for="popular in populars"
-        :popular="popular"
-        :key="popular.id"
-      />
-    </div>
+    <div id="app" class="columns mt-4">
+        <categorize />
+        <br />
+        <div class="column" v-if="noCategorize">
+            
+        </div>
+        <div class="column" v-if="noCategorize">
+        <h3 class="mb-5">이번 주 인기상품</h3>
+            <popular v-for="popular in populars" :popular="popular" :key="popular.id" />
+        </div>
   </div>
 </template>
 
@@ -18,34 +17,35 @@ import Popular from '@/components/Popular.vue';
 import Categorize from '@/components/Categorize.vue';
 
 export default {
-  components: {
-    Popular,
-    Categorize,
-  },
-  data() {
-    return {
-      populars: [
-        {
-          id: 1,
-          name: '바나나 정말 맛있는 바나나',
-          img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
-          info: '100g 3000원',
-        },
-        {
-          id: 2,
-          name: '빵',
-          img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
-          info: '200g 2600원',
-        },
-        {
-          id: 3,
-          name: '우유',
-          img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
-          info: '1L 1800원',
-        },
-      ],
-    };
-  },
+    components: {
+        Popular,
+        Categorize,
+    },
+    data() {
+        return {
+            noCategorize: true,
+            populars: [
+                {
+                    id: 1,
+                    name: "바나나 정말 맛있는 바나나",
+                    img: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+                    info: "100g 3000원",
+                },
+                {
+                    id: 2,
+                    name: "빵",
+                    img: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+                    info: "200g 2600원",
+                },
+                {
+                    id: 3,
+                    name: "우유",
+                    img: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+                    info: "1L 1800원",
+                },
+            ],
+        };
+    },
 };
 </script>
 
