9c7b574dd7d6df24d2bc3d4e1b9321d954c337ec
강민창 <new0822@naver.com>
2021-02-04 09:47:55 +0900

    쇼핑리스트 등록시 shoppingListNo반환

--- backend/springboot/src/main/java/com/springboot/pjt1/repository/mapper/ShoppingListMapper.java
+++ backend/springboot/src/main/java/com/springboot/pjt1/repository/mapper/ShoppingListMapper.java
@@ -6,7 +6,7 @@ import com.springboot.pjt1.repository.dto.ShoppingList;
 
 public interface ShoppingListMapper {
 	public List<ShoppingList> selectShoppingListByMemberId(String MemeberId);
-	public void insertShoppingList(ShoppingList shoppingList);
+	public int insertShoppingList(ShoppingList shoppingList);
 	public void updateShoppingList(ShoppingList shoppingList);
 	public void deleteShoppingListByShoppingListNo(int shoppingListNo);
 }
--- backend/springboot/src/main/java/com/springboot/pjt1/shoppingList/service/ShoppingListServiceImpl.java
+++ backend/springboot/src/main/java/com/springboot/pjt1/shoppingList/service/ShoppingListServiceImpl.java
@@ -20,8 +20,9 @@ public class ShoppingListServiceImpl implements ShoppingListService   {
 	}
 
 	@Override
-	public void insertShoppingList(ShoppingList shoppingList) {
+	public int insertShoppingList(ShoppingList shoppingList) {
 		mapper.insertShoppingList(shoppingList);
+		return shoppingList.getShoppingListNo();
 	}
 
 	@Override
