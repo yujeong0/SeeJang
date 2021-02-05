bb8a9ef638ffd59f7f539e33e6410a884e7a61a0
강유정 <kos9811@naver.com>
2021-02-05 14:10:01 +0900

    db 스크립트 수정

--- backend/springboot/DBscript/shopping.sql
+++ backend/springboot/DBscript/shopping.sql
@@ -24,6 +24,7 @@ DROP TABLE IF EXISTS `best_product`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `best_product` (
   `ranking` int NOT NULL,
+  `product_no` int NOT NULL,
   `product_name` varchar(200) NOT NULL,
   `product_price` varchar(20) NOT NULL,
   PRIMARY KEY (`ranking`)
@@ -36,7 +37,7 @@ CREATE TABLE `best_product` (
 
 LOCK TABLES `best_product` WRITE;
 /*!40000 ALTER TABLE `best_product` DISABLE KEYS */;
-INSERT INTO `best_product` VALUES (1,'CJ 비비고왕교자1.12kg','9,480'),(2,'코카콜라 300ml*12PET','7,980'),(3,'삼립 아침엔 밀크 식빵 330g','2,180'),(4,'냉동 블루베리 1.5kg/봉','10,800'),(5,'빙그레 바나나맛 우유(240ml4개) 960ml','4,580');
+INSERT INTO `best_product` VALUES (1,1,'CJ 비비고왕교자1.12kg','9,480'),(2,20,'코카콜라 300ml*12PET','7,980'),(3,21,'삼립 아침엔 밀크 식빵 330g','2,180'),(4,42,'냉동 블루베리 1.5kg/봉','10,800'),(5,75,'빙그레 바나나맛 우유(240ml4개) 960ml','4,580');
 /*!40000 ALTER TABLE `best_product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -293,4 +294,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-05 13:58:29
+-- Dump completed on 2021-02-05 14:09:45
