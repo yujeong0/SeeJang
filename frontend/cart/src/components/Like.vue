63b4a47b923059cec20832674e66d61cb9a2cdb6
김유리 <youlee602@hanmail.net>
2021-02-05 20:31:48 +0900

    likeproduct

--- frontend/cart/src/components/Like.vue
+++ frontend/cart/src/components/Like.vue
@@ -1,95 +1,63 @@
-<template>
-  <div class="like" id="container">
-    <div class="box">
-      <article class="media">
-        <div class="">
-          <v-container class="tweet">
-            <v-row no-gutters>
-              <v-col class="detailImage">
-                <img class="image" :src="like.img" alt="Image"
-              /></v-col>
-              <v-col class="detail">
-                <strong>{{ like.name }}</strong>
-                {{ like.tweet }}
-                <a :href="like.handle"
-                  ><small>{{ like.handle }}</small></a
-                >
-              </v-col>
-            </v-row>
-          </v-container>
-          <div class="media-content">
-            <div class="content"></div>
-          </div>
+`<template>
+  <div id="container">
+    <div style="text-align: left" class="pl-4 my-1 target ml-3 mr-3">
+        <a target="_blank" :href="like.link"> {{ like.siteName }}</a> :
+        {{ like.productName }}
+        <div class="productPrice" style="text-align: right">
+          {{ like.price }}원
+          <v-btn text outlined @click="del" style="margin-left:7px; width:3%">_
+              <vue-star style="margin-left:" animate="animated bounce" color="#F05654">
+                  <i slot="icon" class="fas fa-trash-alt"></i>
+              </vue-star>
+          </v-btn>
         </div>
-      </article>
-    </div>
+      </div>
   </div>
+
+
 </template>
 
 <script>
+import http from "@/util/http-common.js";
 export default {
   props: {
     like: Object,
+    index: Number
   },
+  created(){
+    console.log(this.index)
+    console.log(this.like)
+  },
+  methods: {
+    async del() {
+      let formData = new FormData();
+      let wishNo = parseInt(this.like.wishNo);
+      formData.append("wishNo",wishNo);
+            await http.post("/wishList/remove", formData ,{
+                withCredentials: true,
+            })
+                .then((response) => {
+                    console.log(response)
+                    this.$emit("del", wishNo);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+    }
+  }
 };
 </script>
 
 <style scoped>
-.button {
-  margin-bottom: 20px;
+.productPrice {
+  color: blue;
 }
-
-.like {
-  max-width: 500px;
-  margin: 0 auto;
-  padding-bottom: 15px;
-}
-
-.box {
-  margin-bottom: 0;
-  border-radius: 0;
-}
-
-.content small {
-  color: #00d1b2;
-}
-
-.image {
-  border-radius: 8px;
-  width: 15%;
-  text-align: left;
-}
-
-.tweet {
-  margin: 0 5%;
-}
-
-.level-item {
-  padding-left: 10px;
-  color: #00d1b2;
-}
-
-input:focus {
-  border-color: #00d1b2;
-}
-
-.likes {
-  padding: 0 7.5px;
-}
-
-input {
-  font-weight: bold;
-}
-
-.likes-move {
-  transition: transform 1s;
-}
-.detailImage {
-  margin-left: -100px;
-}
-.detail {
-  text-align: left;
-  margin-left: -80px;
-  margin-right: 5%;
+.target {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: block;
+  width: 92%;
+  white-space: nowrap;
 }
 </style>
+`
