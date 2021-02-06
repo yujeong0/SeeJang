04c6b0c26fd9a14443939a5e85db4082d893f80a
YSM-samsungNB <cndtjq145@naver.com>
2021-02-06 16:38:06 +0900

    프론트엔드 - 카테고리 타이틀 추가 + 실선 추가

--- frontend/cart/src/components/popular/categoryInfo.vue
+++ frontend/cart/src/components/popular/categoryInfo.vue
@@ -1,23 +1,43 @@
 <template>
     <div id="container">
-        <div style="border-radius: 40px 40px; width: 33%; height: 130px; float: left" class="ma-3">
-            <img
-                :src="require('@/assets/static/productImg/' + this.categoryItem.productNo + '.png')"
-                alt="dd"
-                width="134px"
-                height="140px"
-                style="border-radius: 40px 40px"
-            />
-            <v-btn @click="addShoppingList">쇼핑리스트 추가</v-btn>
+        <v-row no-gutters class="px-5">
+            <v-col class="product">
+                <img
+                    :src="require('@/assets/static/productImg/' + this.categoryItem.productNo + '.png')"
+                    alt="dd"
+                    width="134px"
+                    height="140px"
+                    style="border-radius: 40px 40px"
+                />
+            </v-col>
+            <v-col>
+                <v-row>
+                    <br />
+                </v-row>
+                <v-row no-gutters>
+                    <v-col>
+                        <div class="pro_name" style="text-align: center">
+                            <strong>{{ this.categoryItem.productName }}</strong>
+                        </div>
+                        <div style="text-align: center">
+                            <strong>{{ this.categoryItem.productPrice }} 원</strong>
+                        </div>
+                    </v-col>
+                </v-row>
+                <v-row no-gutters>
+                    <v-col>
+                        <v-btn text outlined @click="addShoppingList">쇼핑리스트 추가 </v-btn>
+                    </v-col>
+                    <v-col style="margin-top: 5px">
+                        <v-btn style="margin-left: 2%" text outlined @click="detail"
+                            >상세 보기
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+        <hr style="margin: 2% 0px" />
         </div>
-        <div
-            style="width: 60%; height: 200px; border: 1px solid white; float: left"
-            class="mt-3 pa-13"
-        >
-            <div>{{ categoryItem.productName }}</div>
-            <div style="color: blue">{{ categoryItem.productPrice }}원</div>
-        </div>
-    </div>
 </template>
 
 <script>
@@ -50,6 +70,17 @@ export default {
                 .catch((error) => {
                     console.log(error);
                 });
+        },
+        detail() {
+            let productInfo = {
+                productName: this.categoryItem.productName,
+                productNo: this.categoryItem.productNo,
+                productPrice: this.categoryItem.productPrice,
+            };
+            console.log("setproductInfo부른다");
+            this.$store.commit("SET_PRODUCT_INFO", { productInfo });
+            console.log("setproductInfo부른뒤");
+            this.$router.push("/detailProduct");
         },
     },
     data() {
