d03337e5cca191c6ee788c26549a49aeae6d87e8
강유정 <kos9811@naver.com>
2021-02-06 17:15:04 +0900

    진짜될것같애

--- frontend/cart/src/util/http-common.js
+++ frontend/cart/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: 'http://springboot:8081/springboot/',
+  baseURL: 'http://i4d109.p.ssafy.io/springboot/',
   headers: {
     'Content-type': 'application/json',
   },
