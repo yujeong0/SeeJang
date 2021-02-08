6aeafbe7c2ca9cd4395ddb3fd1751e3d10068183
정준영 <dkwjdiq@naver.com>
2021-02-08 14:55:52 +0900

    버튼수정

--- frontend/cart/src/components/popular/categoryInfo.vue
+++ frontend/cart/src/components/popular/categoryInfo.vue
@@ -1,93 +1,108 @@
 <template>
-    <div id="container">
-        <v-row no-gutters class="px-5">
-            <v-col class="product">
-                <img
-                    :src="require('@/assets/static/productImg/' + this.categoryItem.productNo + '.png')"
-                    alt="dd"
-                    width="134px"
-                    height="140px"
-                    style="border-radius: 40px 40px"
-                />
-            </v-col>
-            <v-col>
-                <v-row>
-                    <br />
-                </v-row>
-                <v-row no-gutters>
-                    <v-col>
-                        <div class="pro_name" style="text-align: center">
-                            <strong>{{ this.categoryItem.productName }}</strong>
-                        </div>
-                        <div style="text-align: center">
-                            <strong>{{ this.categoryItem.productPrice }} 원</strong>
-                        </div>
-                    </v-col>
-                </v-row>
-                <v-row no-gutters>
-                    <v-col style="text-align:right; padding-left:6%">
-                        <v-btn text outlined @click="addShoppingList">쇼핑리스트 추가 </v-btn>
-                    </v-col>
-                    <v-col style="text-align:left; padding-left:3%" md-4 offset-md="4">
-                        <v-btn style="margin-left: 2%" text outlined @click="detail"
-                            >상세 보기
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-col>
+  <div id="container">
+    <v-row no-gutters class="px-5">
+      <v-col class="product">
+        <img
+          :src="
+            require('@/assets/static/productImg/' +
+              this.categoryItem.productNo +
+              '.png')
+          "
+          alt="dd"
+          width="134px"
+          height="140px"
+          style="border-radius: 40px 40px"
+        />
+      </v-col>
+      <v-col>
+        <v-row>
+          <br />
         </v-row>
-        <hr style="margin: 2% 0px" />
-        </div>
+        <v-row no-gutters>
+          <v-col>
+            <div class="pro_name" style="text-align: center">
+              <strong>{{ this.categoryItem.productName }}</strong>
+            </div>
+            <div style="text-align: center">
+              <strong>{{ this.categoryItem.productPrice }} 원</strong>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col style="text-align: center; padding-left: 6%">
+            <v-btn text outlined style="width: 130px" @click="addShoppingList"
+              >쇼핑리스트 추가
+            </v-btn>
+          </v-col>
+          <v-col
+            style="text-align: center; padding-left: 3%"
+            md-4
+            offset-md="4"
+            class="mt-2"
+          >
+            <v-btn
+              style="margin-left: 2%; width: 130px"
+              text
+              outlined
+              @click="detail"
+              >상세 보기
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <hr style="margin: 2% 0px" />
+  </div>
 </template>
 
 <script>
-import http from "@/util/http-common.js";
+import http from '@/util/http-common.js';
 export default {
-    name: "categoryinfo",
-    props: {
-        categoryItem: Object,
+  name: 'categoryinfo',
+  props: {
+    categoryItem: Object,
+  },
+  created() {
+    console.log('인포페이지');
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    addShoppingList() {
+      let formData = new FormData();
+      formData.append('checked', false);
+      formData.append('memberId', this.$store.getters.getMemberId);
+      formData.append('productName', this.categoryItem.productName);
+      formData.append('productPrice', this.categoryItem.productPrice);
+      http
+        .post('/shoppingList', formData, {
+          withCredentials: true,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    created() {
-        console.log("인포페이지");
+    detail() {
+      let productInfo = {
+        productName: this.categoryItem.productName,
+        productNo: this.categoryItem.productNo,
+        productPrice: this.categoryItem.productPrice,
+      };
+      console.log('setproductInfo부른다');
+      this.$store.commit('SET_PRODUCT_INFO', { productInfo });
+      console.log('setproductInfo부른뒤');
+      this.$router.push('/detailProduct');
     },
-    data() {
-        return {
-        };
-    },
-    methods: {
-        addShoppingList() {
-            let formData = new FormData();
-            formData.append("checked", false);
-            formData.append("memberId", this.$store.getters.getMemberId);
-            formData.append("productName", this.categoryItem.productName);
-            formData.append("productPrice", this.categoryItem.productPrice);
-            http.post("/shoppingList", formData, {
-                withCredentials: true,
-            })
-                .then((response) => {
-                    console.log(response);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        },
-        detail() {
-            let productInfo = {
-                productName: this.categoryItem.productName,
-                productNo: this.categoryItem.productNo,
-                productPrice: this.categoryItem.productPrice,
-            };
-            console.log("setproductInfo부른다");
-            this.$store.commit("SET_PRODUCT_INFO", { productInfo });
-            console.log("setproductInfo부른뒤");
-            this.$router.push("/detailProduct");
-        },
-    },
-    data() {
-        return {
-            //productName: this.$store.getters.getProductName,
-            //   productPrice: this.$store.getters.getProductPrice,
-        };
-    },
+  },
+  data() {
+    return {
+      //productName: this.$store.getters.getProductName,
+      //   productPrice: this.$store.getters.getProductPrice,
+    };
+  },
 };
 </script>
