b2801cf5e25b690f9fc90784897c8eb66d03d17b
강민창 <new0822@naver.com>
2021-02-08 13:56:38 +0900

    develop 수정

--- frontend/cart/src/util/http-common.js
+++ frontend/cart/src/util/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: 'http://i4d109.p.ssafy.io/springboot/',
+  baseURL: 'http://localhost:8081/springboot/',
+  // baseURL: 'http://i4d109.p.ssafy.io/springboot/',
   headers: {
     'Content-type': 'application/json',
   },
