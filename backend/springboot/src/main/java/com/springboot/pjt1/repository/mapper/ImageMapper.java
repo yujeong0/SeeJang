7b4d8006754b63c849dc08e99152bd5b72793a10
강민창 <new0822@naver.com>
2021-02-10 15:53:49 +0900

    이미지 처리응답 dto, mapper 구현

--- backend/springboot/src/main/java/com/springboot/pjt1/repository/mapper/ImageMapper.java
+++ backend/springboot/src/main/java/com/springboot/pjt1/repository/mapper/ImageMapper.java
@@ -1,9 +1,11 @@
 package com.springboot.pjt1.repository.mapper;
 
 import com.springboot.pjt1.repository.dto.Images;
+import com.springboot.pjt1.repository.dto.ImagesResults;
 
 public interface ImageMapper {
 	
 	void insertImage(Images image);
+	ImagesResults selectImageResultsByMemberId(String memberId);
 	
 }
