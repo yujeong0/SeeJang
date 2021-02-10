c1d4e5d4da80beaf0b2133a0be5ee02baa6164c8
강유정 <kos9811@naver.com>
2021-02-10 18:06:32 +0900

    이미지 처리 수정

--- backend/springboot/src/main/java/com/springboot/pjt1/image/controller/ImageController.java
+++ backend/springboot/src/main/java/com/springboot/pjt1/image/controller/ImageController.java
@@ -1,7 +1,5 @@
 package com.springboot.pjt1.image.controller;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +28,7 @@ public class ImageController {
     private ProductService productService;
     
 	@ApiOperation(value = "이미지, 모드, 사용자 ID를 받아 이미지 처리 결과를 반환한다.", 
-			notes = "1번:비장애인용-이미지로 인식한 상품 리스트 반환(List)\n2번:시각장애인용-원하는 물품 위치 반환(String)\n3번:시각장애인용-이미지로 인식한 상품명 반환(String)\nmap의 key는 \"result\"입니다.", 
+			notes = "1번:비장애인용-이미지로 인식한 상품 정보 반환(Product)\n2번:시각장애인용-원하는 물품 위치 반환(String)\n3번:시각장애인용-이미지로 인식한 상품명 반환(String)\nmap의 key는 \"result\"입니다.", 
 			response = List.class)
 	@PostMapping("/searchImage")
 	public Map<String, Object> searchImage(MultipartRequest request) {
@@ -55,7 +53,7 @@ public class ImageController {
 			switch(mode.getOriginalFilename()) {
 			case "1":	// 1  : 일반인
 				resultMap = new HashMap<>();
-				resultMap.put("result", productService.searchProductByName(imageService.getProductName(file, memberId)));
+				resultMap.put("result", productService.selectProductByExactName(imageService.getProductName(file, memberId)));
 				break;
 			case "2":	// 2  : 시각장애인 위치 찾기 음성출력으로 왼쪽, 오른쪽에 있다.
 				String itemName = map.get("item").get(0).getOriginalFilename();	// 찾는 상품이름
