3199487e4283e0b69609e9b2f245d1b7e90a92c2
NICEMANEUL <cndtjq22@gmail.com>
2021-02-11 17:21:16 +0900

    프론트엔드 - ShoppingListNo Null 들어오는거 해결

--- frontend/cart/src/page/login/Login_callback.vue
+++ frontend/cart/src/page/login/Login_callback.vue
@@ -48,6 +48,7 @@ export default {
                     http.post('/user/login/naver', formData, { withCredentials: true })
                          .then((response) => {
                               console.log(response);
+                              this.$router.push('/shoppingList');
                          })
                          .catch((error) => {
                               console.log(error);
@@ -57,7 +58,6 @@ export default {
                     alert('문제가 발생했습니다! 다시 로그인 해주세요!');
                     this.$router.push('/login');
                }
-               this.$router.push('/shoppingList');
           });
      },
 };
