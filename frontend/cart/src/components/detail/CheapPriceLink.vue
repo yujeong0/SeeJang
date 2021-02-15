bdf4d2f7deab322d659c9b5d744e6f890d22d4fa
정준영 <dkwjdiq@naver.com>
2021-02-15 10:16:34 +0900

    시각장애인용 카메라 마진 수정(헤더 마진 안들어가게)

--- frontend/cart/src/components/detail/CheapPriceLink.vue
+++ frontend/cart/src/components/detail/CheapPriceLink.vue
@@ -108,6 +108,8 @@ export default {
       })
       .then((response) => {
         // this.datas = resopnse.data.bestPrice;
+        console.log(response);
+        console.log(response.data.bestPrice.length);
 
         for (let i = 0; i < response.data.bestPrice.length; i++) {
           this.datas.push(response.data.bestPrice[i]);
