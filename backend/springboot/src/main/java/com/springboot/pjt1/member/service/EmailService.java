fa9ccc29bc8836af8145a755483771239357d0a0
정준영 <dkwjdiq@naver.com>
2021-03-17 12:13:02 +0900

    clone SEE Jang

--- backend/springboot/src/main/java/com/springboot/pjt1/member/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.pjt1.member.service;
-
-public interface EmailService {
-
-    void sendMail(String to, String sub, String text);
-}
--- backend/springboot/src/main/java/com/springboot/pjt1/repository/jpa/SaltRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.pjt1.repository.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.springboot.pjt1.repository.jpa.dto.Salt;
-
-public interface SaltRepository extends CrudRepository<Salt,Long> {
-
-}
